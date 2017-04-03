b70af03fd2010e7ae0cb0d21b5230f658b2b5861
Eric Lee <e.keith.lee@gmail.com>
2017-04-03 14:18:43 -0700

    fixed paralax. Took out the hidden animation

--- client/scripts/jquery.js
+++ client/scripts/jquery.js
@@ -17,38 +17,38 @@ $(document).ready(function(){
 	    if ($(this).scrollTop() > 800) {
 	        $('#left').removeClass("hidden").addClass("animated once rollIn");
 	    }
-	    if ($(this).scrollTop() > 2000 && $(this).scrollTop() < 2450) {
+	    // if ($(this).scrollTop() > 2000 && $(this).scrollTop() < 2450) {
 	    	
-	    	$('#proj1').removeClass("hidden").addClass("animated once flipInX");
-	    }
-	    else if ($(this).scrollTop() > 3400 || $(this).scrollTop() < 1800) {
+	    // 	$('#proj1').removeClass("hidden").addClass("animated once flipInX");
+	    // }
+	    // else if ($(this).scrollTop() > 3400 || $(this).scrollTop() < 1800) {
 	    	
-	    	$('#proj1').removeClass("animated once flipInX").addClass("hidden");
-	    }
-	    if ($(this).scrollTop() > 2050 && $(this).scrollTop() < 2450) {
+	    // 	$('#proj1').removeClass("animated once flipInX").addClass("hidden");
+	    // }
+	    // if ($(this).scrollTop() > 2050 && $(this).scrollTop() < 2450) {
 	    	
-	    	$('#proj2').removeClass("hidden").addClass("animated once flipInX");
-	    }
-	    else if ($(this).scrollTop() > 3400 || $(this).scrollTop() < 1800) {
+	    // 	$('#proj2').removeClass("hidden").addClass("animated once flipInX");
+	    // }
+	    // else if ($(this).scrollTop() > 3400 || $(this).scrollTop() < 1800) {
 	    
-	    	$('#proj2').removeClass("animated once flipInX").addClass("hidden");
-	    }
-	    if ($(this).scrollTop() > 2100 && $(this).scrollTop() < 2450) {
+	    // 	$('#proj2').removeClass("animated once flipInX").addClass("hidden");
+	    // }
+	    // if ($(this).scrollTop() > 2100 && $(this).scrollTop() < 2450) {
 
-	    	$('#proj3').removeClass("hidden").addClass("animated once flipInX");
-	    }
-	    else if ($(this).scrollTop() > 3400 || $(this).scrollTop() < 1800) {
+	    // 	$('#proj3').removeClass("hidden").addClass("animated once flipInX");
+	    // }
+	    // else if ($(this).scrollTop() > 3400 || $(this).scrollTop() < 1800) {
 	    	
-	    	$('#proj3').removeClass("animated once flipInX").addClass("hidden");
-	    }
-	    if ($(this).scrollTop() > 2100 && $(this).scrollTop() < 2450) {
+	    // 	$('#proj3').removeClass("animated once flipInX").addClass("hidden");
+	    // }
+	    // if ($(this).scrollTop() > 2100 && $(this).scrollTop() < 2450) {
 
-	    	$('#proj4').removeClass("hidden").addClass("animated once flipInX");
-	    }
-	    else if ($(this).scrollTop() > 3400 || $(this).scrollTop() < 1800) {
+	    // 	$('#proj4').removeClass("hidden").addClass("animated once flipInX");
+	    // }
+	    // else if ($(this).scrollTop() > 3400 || $(this).scrollTop() < 1800) {
 	    	
-	    	$('#proj4').removeClass("animated once flipInX").addClass("hidden");
-	    }
+	    // 	$('#proj4').removeClass("animated once flipInX").addClass("hidden");
+	    // }
 
 	    if ($(window).scrollTop() != 0){
 	    	//console.log("IM scrolling");
